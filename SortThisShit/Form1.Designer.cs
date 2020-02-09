6477d04a302f8665c7c73e2672bda50f72175e4e
Owendho <36518961+Owendho@users.noreply.github.com>
2020-02-09 19:48:27 +0100

    Bubblesort

---
+++ SortThisShit/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace SortThisShit
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnOne = new System.Windows.Forms.Button();
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.UnsortedList = new System.Windows.Forms.ListBox();
+            this.sortedList = new System.Windows.Forms.ListBox();
+            this.txtBox2 = new System.Windows.Forms.TextBox();
+            this.btnMerge = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtTimer = new System.Windows.Forms.TextBox();
+            this.lblArraystørelse = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOne
+            // 
+            this.btnOne.Location = new System.Drawing.Point(241, 11);
+            this.btnOne.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnOne.Name = "btnOne";
+            this.btnOne.Size = new System.Drawing.Size(116, 92);
+            this.btnOne.TabIndex = 0;
+            this.btnOne.Text = "buttblesort";
+            this.btnOne.UseVisualStyleBackColor = true;
+            this.btnOne.Click += new System.EventHandler(this.btnOne_Click);
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Location = new System.Drawing.Point(241, 150);
+            this.txtBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(117, 20);
+            this.txtBox1.TabIndex = 1;
+            // 
+            // UnsortedList
+            // 
+            this.UnsortedList.FormattingEnabled = true;
+            this.UnsortedList.Location = new System.Drawing.Point(28, 11);
+            this.UnsortedList.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.UnsortedList.Name = "UnsortedList";
+            this.UnsortedList.Size = new System.Drawing.Size(110, 238);
+            this.UnsortedList.TabIndex = 2;
+            // 
+            // sortedList
+            // 
+            this.sortedList.FormattingEnabled = true;
+            this.sortedList.Location = new System.Drawing.Point(481, 18);
+            this.sortedList.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.sortedList.Name = "sortedList";
+            this.sortedList.Size = new System.Drawing.Size(102, 238);
+            this.sortedList.TabIndex = 3;
+            // 
+            // txtBox2
+            // 
+            this.txtBox2.Location = new System.Drawing.Point(241, 209);
+            this.txtBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtBox2.Name = "txtBox2";
+            this.txtBox2.Size = new System.Drawing.Size(117, 20);
+            this.txtBox2.TabIndex = 4;
+            // 
+            // btnMerge
+            // 
+            this.btnMerge.Location = new System.Drawing.Point(241, 264);
+            this.btnMerge.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnMerge.Name = "btnMerge";
+            this.btnMerge.Size = new System.Drawing.Size(116, 74);
+            this.btnMerge.TabIndex = 5;
+            this.btnMerge.Text = "MergeSort";
+            this.btnMerge.UseVisualStyleBackColor = true;
+            this.btnMerge.Click += new System.EventHandler(this.btnMerge_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // txtTimer
+            // 
+            this.txtTimer.Location = new System.Drawing.Point(28, 292);
+            this.txtTimer.Name = "txtTimer";
+            this.txtTimer.Size = new System.Drawing.Size(100, 20);
+            this.txtTimer.TabIndex = 6;
+            // 
+            // lblArraystørelse
+            // 
+            this.lblArraystørelse.AutoSize = true;
+            this.lblArraystørelse.Location = new System.Drawing.Point(153, 212);
+            this.lblArraystørelse.Name = "lblArraystørelse";
+            this.lblArraystørelse.Size = new System.Drawing.Size(83, 13);
+            this.lblArraystørelse.TabIndex = 7;
+            this.lblArraystørelse.Text = "Størelse af array";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 384);
+            this.Controls.Add(this.lblArraystørelse);
+            this.Controls.Add(this.txtTimer);
+            this.Controls.Add(this.btnMerge);
+            this.Controls.Add(this.txtBox2);
+            this.Controls.Add(this.sortedList);
+            this.Controls.Add(this.UnsortedList);
+            this.Controls.Add(this.txtBox1);
+            this.Controls.Add(this.btnOne);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOne;
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.ListBox UnsortedList;
+        private System.Windows.Forms.ListBox sortedList;
+        private System.Windows.Forms.TextBox txtBox2;
+        private System.Windows.Forms.Button btnMerge;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox txtTimer;
+        private System.Windows.Forms.Label lblArraystørelse;
+    }
+}
+
---
+++ SortThisShit/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Timers;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SortThisShit
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        List<int> MitArray = new List<int>();
+
+        private int ticks;
+
+        private void btnOne_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            
+
+
+
+            //fjerner tekst i vores listbox of clear vores array så man ikke behøver at luk programmet hver eneste gang.
+            sortedList.Items.Clear();
+            MitArray.Clear();
+            //int Range; 
+            int length = int.Parse(txtBox1.Text);
+            int max = int.Parse(txtBox2.Text);
+            var rand = new Random();
+            //string reset = sortedList.Text;
+            //reset = " ";
+            //Range.Add(længde);
+            //MitArray.AddRange();
+
+
+            //generer fra 0 til hvad brugeren har valgt.
+            for (int i = 0; i < length; i++)
+
+            {
+                MitArray.Add(rand.Next(0, max));
+
+            }
+
+            int temp;
+
+
+            for (int j = 0; j <= MitArray.Count - 2; j++)
+            {
+                for (int i = 0; i <= MitArray.Count - 2; i++)
+                {
+                    if (MitArray[i] > MitArray[i + 1])
+                    {
+                        temp = MitArray[i + 1];
+                        MitArray[i + 1] = MitArray[i];
+                        MitArray[i] = temp;
+                    }
+                }
+            }
+
+            //printer vores sorted array ud i vores sorted listbox
+            for (int p = 0; p < MitArray.Count; p++)
+            {
+                sortedList.Items.Add(MitArray[p].ToString());
+            }
+
+            /*foreach (int p in MitArray)
+            {
+
+                sortedList.Items.Add(MitArray[p].ToString());
+            }*/
+
+
+
+
+        }
+
+        private void btnMerge_Click(object sender, EventArgs e)
+        {
+            sortedList.Items.Clear();
+            MitArray.Clear();
+            int length = int.Parse(txtBox1.Text);
+            int max = int.Parse(txtBox2.Text);
+            var rand = new Random();
+
+            List<int> unsorted = new List<int>();
+            List<int> sorted;
+
+            for (int i = 0; i < length; i++)
+            {
+                unsorted.Add(rand.Next(0, max));
+            }
+
+            sorted = Mergesort(unsorted);
+
+            foreach (int item in sorted)
+            {
+                sortedList.Items.Add(sorted.ToString());
+
+            }
+
+            /*for (int i = 0; i < MitArray.Count / 2; i++)
+			{
+                
+
+			}
+            */
+
+
+        }
+
+
+        //kilde til noget af koden: https://www.w3resource.com/csharp-exercises/searching-and-sorting-algorithm/searching-and-sorting-algorithm-exercise-7.php
+        //laver en funktion som bliver kaldt i knappen. Denne funktion er til at splitte numrene op så de kan klive sorteret efter.
+        private static List<int> Mergesort(List<int> unsorted)
+        {         
+
+            if (unsorted.Count <= 0)
+            {
+                return unsorted;
+            }
+
+            List<int> left = new List<int>();
+            List<int> right = new List<int>();
+
+            //Når unsorted bliver divideret med 2 bliver den 5, men når jeg sætter den ind i en int variable, bliver variablen 0. Why?
+            int middle = unsorted.Count / 2;
+
+            for (int i = 0; i < middle; i++)
+            {
+                left.Add(unsorted[i]);
+            }
+
+            for (int i = 0; i < unsorted.Count; i++)
+            {
+                right.Add(unsorted[i]);
+
+
+            }
+
+            left = Mergesort(left);
+            right = Mergesort(right);
+            return merge(left, right);
+
+        }
+
+        //Mergesort funktionen har splittet alle vores elementer. Nu laver vi en funktion som sortere dem og merger dem.
+        private static List<int> merge(List<int> right, List<int> left)
+        {
+            List<int> result = new List<int>();
+
+            while (left.Count > 0 || right.Count > 0)
+            {
+                if (left.Count > 0 && right.Count > 0)
+                {
+                    if (left.First() <= right.First())
+                    {
+                        result.Add(left.First());
+                        right.Remove(left.First());
+
+
+                    }
+                    else
+                    {
+                        result.Add(right.First());
+                        right.Remove(right.First());
+
+                    }
+                }
+                else if (left.Count > 0)
+                {
+                    result.Add(left.First());
+                    left.Remove(left.First());
+                }
+                else if (right.Count > 0)
+                {
+                    result.Add(right.First());
+                    right.Remove(right.First());
+                }
+
+            }
+            return result;
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            ticks++;
+            txtTimer.Text = ticks.ToString();
+
+
+
+        }
+    }
+}
