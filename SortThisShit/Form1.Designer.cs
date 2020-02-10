1426f0eeff2cb68f9cfcebb3ced4030cf9d37a17
Owendho <36518961+Owendho@users.noreply.github.com>
2020-02-10 23:15:56 +0100

    Sorterings program
    Bubblesort viker. Har ikke fået mergesort til at virke. Kan ikke printe data til excel

--- SortThisShit/Form1.Designer.cs
+++ SortThisShit/Form1.Designer.cs
@@ -38,12 +38,15 @@
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.txtTimer = new System.Windows.Forms.TextBox();
             this.lblArraystørelse = new System.Windows.Forms.Label();
+            this.lblMS = new System.Windows.Forms.Label();
+            this.lblAntalTal = new System.Windows.Forms.Label();
+            this.btnExcel = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnOne
             // 
             this.btnOne.Location = new System.Drawing.Point(241, 11);
-            this.btnOne.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnOne.Margin = new System.Windows.Forms.Padding(2);
             this.btnOne.Name = "btnOne";
             this.btnOne.Size = new System.Drawing.Size(116, 92);
             this.btnOne.TabIndex = 0;
@@ -54,7 +57,7 @@
             // txtBox1
             // 
             this.txtBox1.Location = new System.Drawing.Point(241, 150);
-            this.txtBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtBox1.Margin = new System.Windows.Forms.Padding(2);
             this.txtBox1.Name = "txtBox1";
             this.txtBox1.Size = new System.Drawing.Size(117, 20);
             this.txtBox1.TabIndex = 1;
@@ -63,7 +66,7 @@
             // 
             this.UnsortedList.FormattingEnabled = true;
             this.UnsortedList.Location = new System.Drawing.Point(28, 11);
-            this.UnsortedList.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.UnsortedList.Margin = new System.Windows.Forms.Padding(2);
             this.UnsortedList.Name = "UnsortedList";
             this.UnsortedList.Size = new System.Drawing.Size(110, 238);
             this.UnsortedList.TabIndex = 2;
@@ -72,7 +75,7 @@
             // 
             this.sortedList.FormattingEnabled = true;
             this.sortedList.Location = new System.Drawing.Point(481, 18);
-            this.sortedList.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.sortedList.Margin = new System.Windows.Forms.Padding(2);
             this.sortedList.Name = "sortedList";
             this.sortedList.Size = new System.Drawing.Size(102, 238);
             this.sortedList.TabIndex = 3;
@@ -80,7 +83,7 @@
             // txtBox2
             // 
             this.txtBox2.Location = new System.Drawing.Point(241, 209);
-            this.txtBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtBox2.Margin = new System.Windows.Forms.Padding(2);
             this.txtBox2.Name = "txtBox2";
             this.txtBox2.Size = new System.Drawing.Size(117, 20);
             this.txtBox2.TabIndex = 4;
@@ -88,7 +91,7 @@
             // btnMerge
             // 
             this.btnMerge.Location = new System.Drawing.Point(241, 264);
-            this.btnMerge.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnMerge.Margin = new System.Windows.Forms.Padding(2);
             this.btnMerge.Name = "btnMerge";
             this.btnMerge.Size = new System.Drawing.Size(116, 74);
             this.btnMerge.TabIndex = 5;
@@ -117,11 +120,42 @@
             this.lblArraystørelse.TabIndex = 7;
             this.lblArraystørelse.Text = "Størelse af array";
             // 
+            // lblMS
+            // 
+            this.lblMS.AutoSize = true;
+            this.lblMS.Location = new System.Drawing.Point(134, 295);
+            this.lblMS.Name = "lblMS";
+            this.lblMS.Size = new System.Drawing.Size(20, 13);
+            this.lblMS.TabIndex = 8;
+            this.lblMS.Text = "ms";
+            // 
+            // lblAntalTal
+            // 
+            this.lblAntalTal.AutoSize = true;
+            this.lblAntalTal.Location = new System.Drawing.Point(179, 153);
+            this.lblAntalTal.Name = "lblAntalTal";
+            this.lblAntalTal.Size = new System.Drawing.Size(57, 13);
+            this.lblAntalTal.TabIndex = 9;
+            this.lblAntalTal.Text = "Antal af tal";
+            // 
+            // btnExcel
+            // 
+            this.btnExcel.Location = new System.Drawing.Point(425, 264);
+            this.btnExcel.Name = "btnExcel";
+            this.btnExcel.Size = new System.Drawing.Size(106, 74);
+            this.btnExcel.TabIndex = 10;
+            this.btnExcel.Text = "Print til excel";
+            this.btnExcel.UseVisualStyleBackColor = true;
+            this.btnExcel.Click += new System.EventHandler(this.BtnExcel_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(610, 384);
+            this.Controls.Add(this.btnExcel);
+            this.Controls.Add(this.lblAntalTal);
+            this.Controls.Add(this.lblMS);
             this.Controls.Add(this.lblArraystørelse);
             this.Controls.Add(this.txtTimer);
             this.Controls.Add(this.btnMerge);
@@ -130,7 +164,7 @@
             this.Controls.Add(this.UnsortedList);
             this.Controls.Add(this.txtBox1);
             this.Controls.Add(this.btnOne);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -149,6 +183,9 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.TextBox txtTimer;
         private System.Windows.Forms.Label lblArraystørelse;
+        private System.Windows.Forms.Label lblMS;
+        private System.Windows.Forms.Label lblAntalTal;
+        private System.Windows.Forms.Button btnExcel;
     }
 }
 
--- SortThisShit/Form1.cs
+++ SortThisShit/Form1.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Timers;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using Excel = Microsoft.Office.Interop.Excel;
+
 
 namespace SortThisShit
 {
@@ -21,14 +17,13 @@ namespace SortThisShit
 
         List<int> MitArray = new List<int>();
 
-        private int ticks;
 
         private void btnOne_Click(object sender, EventArgs e)
         {
-            timer1.Start();
-            
 
-
+            //Laver en timer som skal printe hvor lang tid programmet bruger på kør igennem
+            Stopwatch Timer = new Stopwatch();
+            Timer.Start();
 
             //fjerner tekst i vores listbox of clear vores array så man ikke behøver at luk programmet hver eneste gang.
             sortedList.Items.Clear();
@@ -37,10 +32,6 @@ namespace SortThisShit
             int length = int.Parse(txtBox1.Text);
             int max = int.Parse(txtBox2.Text);
             var rand = new Random();
-            //string reset = sortedList.Text;
-            //reset = " ";
-            //Range.Add(længde);
-            //MitArray.AddRange();
 
 
             //generer fra 0 til hvad brugeren har valgt.
@@ -53,7 +44,7 @@ namespace SortThisShit
 
             int temp;
 
-
+            //Bubblesort kode
             for (int j = 0; j <= MitArray.Count - 2; j++)
             {
                 for (int i = 0; i <= MitArray.Count - 2; i++)
@@ -67,22 +58,21 @@ namespace SortThisShit
                 }
             }
 
-            //printer vores sorted array ud i vores sorted listbox
+            //printer vores sorted array fra bubblesort ud i vores sorted listbox
             for (int p = 0; p < MitArray.Count; p++)
             {
                 sortedList.Items.Add(MitArray[p].ToString());
             }
 
-            /*foreach (int p in MitArray)
-            {
+            //Stopper vores timer.
+            Timer.Stop();
 
-                sortedList.Items.Add(MitArray[p].ToString());
-            }*/
-
-
+            //Sørger for at 
+            txtTimer.Text = Timer.ElapsedMilliseconds.ToString();
 
 
         }
+
 
         private void btnMerge_Click(object sender, EventArgs e)
         {
@@ -100,104 +90,37 @@ namespace SortThisShit
                 unsorted.Add(rand.Next(0, max));
             }
 
-            sorted = Mergesort(unsorted);
-
-            foreach (int item in sorted)
-            {
-                sortedList.Items.Add(sorted.ToString());
-
-            }
-
-            /*for (int i = 0; i < MitArray.Count / 2; i++)
-			{
-                
-
-			}
-            */
-
+            //sorted = Mergesort(unsorted);
 
         }
 
 
-        //kilde til noget af koden: https://www.w3resource.com/csharp-exercises/searching-and-sorting-algorithm/searching-and-sorting-algorithm-exercise-7.php
-        //laver en funktion som bliver kaldt i knappen. Denne funktion er til at splitte numrene op så de kan klive sorteret efter.
+        //Start på et forberet mergesort program da det andet ikke virkede. Det andet kode ligger i et github branch
         private static List<int> Mergesort(List<int> unsorted)
-        {         
-
-            if (unsorted.Count <= 0)
-            {
-                return unsorted;
-            }
-
-            List<int> left = new List<int>();
-            List<int> right = new List<int>();
-
-            //Når unsorted bliver divideret med 2 bliver den 5, men når jeg sætter den ind i en int variable, bliver variablen 0. Why?
-            int middle = unsorted.Count / 2;
-
-            for (int i = 0; i < middle; i++)
-            {
-                left.Add(unsorted[i]);
-            }
-
-            for (int i = 0; i < unsorted.Count; i++)
-            {
-                right.Add(unsorted[i]);
-
-
-            }
-
-            left = Mergesort(left);
-            right = Mergesort(right);
-            return merge(left, right);
-
-        }
-
-        //Mergesort funktionen har splittet alle vores elementer. Nu laver vi en funktion som sortere dem og merger dem.
-        private static List<int> merge(List<int> right, List<int> left)
         {
-            List<int> result = new List<int>();
+            //Laver to lister som skal indeholde de divideret elementer fra unsorted lsit
+            int middle;
+            List<int> R = new List<int>();
+            List<int> L = new List<int>();
 
-            while (left.Count > 0 || right.Count > 0)
+            if (R.Count < L.Count)
             {
-                if (left.Count > 0 && right.Count > 0)
-                {
-                    if (left.First() <= right.First())
-                    {
-                        result.Add(left.First());
-                        right.Remove(left.First());
+                middle = (R.Count + L.Count) / 2;
 
 
-                    }
-                    else
-                    {
-                        result.Add(right.First());
-                        right.Remove(right.First());
-
-                    }
-                }
-                else if (left.Count > 0)
-                {
-                    result.Add(left.First());
-                    left.Remove(left.First());
-                }
-                else if (right.Count > 0)
-                {
-                    result.Add(right.First());
-                    right.Remove(right.First());
-                }
 
             }
-            return result;
 
+            return unsorted;
         }
 
         private void Timer1_Tick(object sender, EventArgs e)
         {
-            ticks++;
-            txtTimer.Text = ticks.ToString();
 
+        }
 
+        private void BtnExcel_Click(object sender, EventArgs e)
+        {
 
         }
     }
